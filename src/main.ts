a87c4ec9a3a3e51d2b5e21295dc82b5db339f6b7
Shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2025-02-16 21:29:43 +0500

    fixed controller auth

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ async function start() {
   const PORT = process.env.PORT || 3005;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api');
   await app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
 }
 
