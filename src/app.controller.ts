9a3efbc0380e7fe31ac9363cdd380c219834bbdd
Shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2025-02-16 10:39:37 +0500

    added privacy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,17 @@ import * as path from 'path';
 
 @Controller()
 export class AppController {
+  @Get('privacy-policy')
+  getPrivacyPolicy(@Res() res: Response) {
+    const filePath = path.join(
+      __dirname,
+      '..',
+      'public',
+      'privacy-policy.html',
+    );
+    return res.sendFile(filePath);
+  }
+
   @Get('data-deletion')
   getDataDeletion(@Res() res: Response) {
     const filePath = path.join(__dirname, '..', 'public', 'data-deletion.html');
